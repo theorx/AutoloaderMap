06dd481198f0cbf859fe66f292c505220ebb19ec
Lauri Orgla <theorx@hotmail.com>
2016-06-15 22:03:33 +0300

    Added empty test and improved .gitignore

---
+++ tests/Unit/library/AutoLoaderMapTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit\Theorx\AutoLoaderMap;
+
+/**
+ * Class AutoLoaderMapTest
+ *
+ * @author             Lauri Orgla <theorx@hotmail.com>
+ * @coversDefaultClass Theorx\AutoLoaderMap\AutoLoaderMap
+ * @package            Tests\Unit\Theorx\AutoLoaderMap
+ */
+class AutoLoaderMapTest extends \PHPUnit_Framework_TestCase {
+
+    //todo: implement tests
+    /**
+     * @author Lauri Orgla <theorx@hotmail.com>
+     */
+    public function testEmptyTest() {
+
+        $this->assertTrue(true);
+    }
+}
